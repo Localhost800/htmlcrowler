899b29f9811323ec9d6173fa9b2683661a32029a
Sakhawath Hossen Sohan <Localhost800@users.noreply.github.com>
2015-04-06 20:07:56 +0600

    Create settings.py

---
+++ htmlcrowler/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for htmlcrowler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'htmlcrowler'
+
+SPIDER_MODULES = ['htmlcrowler.spiders']
+NEWSPIDER_MODULE = 'htmlcrowler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'htmlcrowler (+http://www.yourdomain.com)'
